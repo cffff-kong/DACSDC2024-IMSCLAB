657d96ac4bfcba11feae2304a1ebcad9abd6e6fe
LI <jklee@connect.hku.hk>
2024-04-29 15:49:27 +0800

    push nano

---
+++ segformer-nano-github/nets/backbone_mit.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+from nets.mit import mit_b0
+
+from mmcv.runner import load_checkpoint
+
+
+
+class yolo_mit(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.backbone = mit_b0()
+        
+
+
+
+    def forward(self, x):
+        conv1, conv2, conv3, conv4 = self.backbone(x)
+
+        # return conv1, conv2, conv3, conv4
+        return conv2, conv3, conv4
+
+
+
+
+
+    
+    
